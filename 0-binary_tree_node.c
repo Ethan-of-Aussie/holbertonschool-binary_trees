226e7ec92f83a398019673a142b81232c7ab3432
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-04-04 04:52:07 +0000

    possibly correct

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,5 +1,13 @@
 #include "binary_trees.h"
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
+	binary_tree_t *node = malloc(sizeof(*node));
 
+	node->parent = parent;
+	node->n = value;
+
+	if (node != NULL)
+	return (node);
+
+return (NULL);
 }
