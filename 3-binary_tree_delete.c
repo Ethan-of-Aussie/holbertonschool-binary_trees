37a10df119f9ea4d2f597b02aac76b3a649d46b7
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-04-04 07:13:54 +0000

    checker T.3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,5 +1,13 @@
 #include "binary_trees.h"
+/**
+ *binary_tree_delete - Time to delete
+ *@tree: Pointer to binary tree 
+ */
 void binary_tree_delete(binary_tree_t *tree)
 {
-
+	if (!tree)
+	return;
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
 }
