a5866ab3487341f59ecb653cf650cdf3a6395519
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-04-04 07:04:38 +0000

    checker T.2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,5 +1,35 @@
 #include "binary_trees.h"
+/**
+ *binary_tree_insert_right - Create and insert right nodes
+ *
+ *@parent: Parent is pointer to struct node parent
+ *@value: The value of n from struct
+ *Return: NULL if failure, node if complete creation
+ */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
+binary_tree_t *node = malloc(sizeof(*node));
+binary_tree_t *tmp;
 
+	if (!node)
+		return (NULL);
+	if (!parent)
+	{
+		free(node);
+		return (NULL);
+	}
+	tmp = parent->right;
+	node->parent = parent;
+	node->left = NULL;
+	node->right = NULL;
+	node->n = value;
+	parent->right = node;
+
+	if (tmp != NULL)
+	{
+	node->right = tmp;
+	tmp->parent = node;
+	}
+
+	return (node);
 }
