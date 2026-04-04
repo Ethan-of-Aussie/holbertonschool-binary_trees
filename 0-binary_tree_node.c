2534eaefa9fe648ec5659299e344635038db0ccc
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-04-04 04:55:24 +0000

    possibly correct

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,4 +1,11 @@
 #include "binary_trees.h"
+/**
+ *binary_tree_node - Creates binary tree node
+ *
+ *@parent: Parent is pointer to struct node parent
+ *@value: The value of n from struct
+ *Return: NULL if failur, node if complete creation
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node = malloc(sizeof(*node));
