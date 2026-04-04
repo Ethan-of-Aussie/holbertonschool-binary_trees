fb702300fd0c9833825c2cdd151017ad2bf663bb
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-04-04 06:10:47 +0000

    save

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -4,15 +4,12 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 binary_tree_t *node = malloc(sizeof(*node));
 	if (!node)
 		return (NULL);
-	node->left = NULL;
+	if (node != NULL)
+	{
+	node->left = parent;
 	node->right = NULL;
 	node->n = value;
-
-	if (value < parent->n)
-	  {
-	    node = node->left;
-	    if (!node)
-		return (node);
-	  }
+	return (node);
+	}
 	return (NULL);
 }
