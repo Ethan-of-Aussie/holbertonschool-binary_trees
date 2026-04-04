29d7ee61e9d45f8e97c0b30d0af3ea9cd00833d9
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-04-04 05:11:21 +0000

    testing checker, possibly finished

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,17 +4,21 @@
  *
  *@parent: Parent is pointer to struct node parent
  *@value: The value of n from struct
- *Return: NULL if failur, node if complete creation
+ *Return: NULL if failure, node if complete creation
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node = malloc(sizeof(*node));
 
+	if (!node)
+		return (NULL);
 	node->parent = parent;
+	node->left = NULL;
+	node->right = NULL;
 	node->n = value;
 
 	if (node != NULL)
 	return (node);
 
-return (0);
+return (NULL);
 }
