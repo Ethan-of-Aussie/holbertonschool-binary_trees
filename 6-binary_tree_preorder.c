47b76650d72c63a674f38b8287f169a18a93f3df
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-04-05 05:25:49 +0000

    checker T.6

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,4 +1,9 @@
 #include "binary_trees.h"
+/**
+ *binary_tree_preorder - binary tree using pre-order traversal
+ *@tree: Pointer to root of a tree
+ *@func: func points to a function to call nodes
+ */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree != NULL)
@@ -7,5 +12,4 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	binary_tree_preorder(tree->left, func);
 	binary_tree_preorder(tree->right, func);
 	}
-	return;
 }
