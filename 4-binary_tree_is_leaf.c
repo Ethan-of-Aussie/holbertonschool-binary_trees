b97c767c86f4fc3267b4cad74e96e80216f87358
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-04-05 04:29:39 +0000

    checker T.4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,6 +1,7 @@
 #include "binary_trees.h"
 /**
  *binary_tree_is_leaf - Check if node is leaf
+ *@node: Pointer to binary_tree_t node to check
  *Return: 1 on found leaf or 0 if not
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
