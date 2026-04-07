874f81422eb24c091b229726490294cff86c43f5
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-04-07 13:20:09 +0000

    test

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -2,7 +2,13 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int lh;
+	int rh;
+
   	if (!tree)
 	return (0);
-	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+
+	lh = binary_tree_height(tree->left);
+	rh = binary_tree_height(tree->right);
+	return (1 + (lh > rh ? lh : rh));
 }
